b085ce27703a76216d379cc1b1886f4faf56e7c1
pakuhn <pakuhn93@gmail.com>
2023-06-04 20:12:06 -0700

    base files for challenge

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Storing Search History
+// WHEN i input a city
+// THEN i put that city into an object that holds my search history
+// THEN i display a button on the screen with that city's name
+// THEN i put it into localStorage for future use
+// OPTIONAL: create a reset storage button that clears the object of its properties
+
+// 5-Day Forecast
+// on the UI, the user will see 5 cards with a header of 5-Day Forecast
+// these 5 cards will store a Date, Weather Type (such as clear skies), Temperature, Wind Speed, and Humidity
+    // the units of strage will be Farenheit, MPH, and %
+    // the information will be extracted from the weather API that is given to us
+// the base cards will hold placeholder values of '-' or 'N/A'
+// WHEN i input a city
+// THEN the program will fetch the data values needed for my cards and display it to the user
+    // the current day will be larger than the future dates
